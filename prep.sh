a025542aab54fba9c00c1f51347d2db3bd50c720
radmir <radmir@pop-os.localdomain>
2022-10-18 21:33:41 +0600

    initial commit

--- prep.sh
+++ prep.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
+sleep 2
+echo ""
 cd ~/Desktop
 mkdir amc_py
+echo "Creating folder for AMC on Desktop"
+sleep 1
+echo ""
 cd amc_py
+echo "Starting to clone repository and prepping the files"
+sleep 1
+echo ""
+sleep 1
 git clone https://github.com/mirakklys/python_for_AMC.git
-rm -rf .git READMR.md Auto Multiple Choice Instructions.pdf prep.sh
+echo ""
+sleep 1
+echo "Downloading complete"
+sleep 1
+echo ""
+cd python_for_AMC
+rm -rf .git README.md Auto\ Multiple\ Choice\ Instructions.pdf prep.sh
+echo "Removing unnecessary files"
+sleep 1
+echo ""
+echo "Moving necessary files to amc-py folder"
 mv * ..
 cd ..
+echo ""
 rm -rf python_for_AMC
+sleep 1
+echo "Job done!"
+sleep 2
 exit
