7de0948febd673d74196dc9c590022af73d48762
mirakklys <radmir.surg@gmail.com>
2022-10-20 17:45:33 +0600

    1.2.1

--- start.sh
+++ start.sh
@@ -2,15 +2,19 @@
 # Echo -e is to show a special formating of the following string
 weAreHere=$(pwd)
 echo -e '\033[1mHello, this is an interactive prep of AMC'
+
 # Sleep :)
 sleep 2
 echo -e "Please name your Exam (without spaces): \033[0m"
+
 # Read -r to wait for a response and write it to exam_name variable
 read -r exam_name
+
 # Use the variable for naming a folder for this specific exam 
 mkdir ~/MC-Projects/${exam_name}
 echo -e "\033[1mCreating folder for \033[45m$exam_name\033[0m \033[1mAMC in \033[45m~/MC-Projects\033[0m"
 sleep 1
+
 # Decorations, fshuuuh... $COLUMNS is to show the width of terminal and feed it to printf | tr to swap spaces with tilda
 printf %"$COLUMNS"s |tr " " "~"
 echo -n -e '''\033[1mIs this the first time of using AMC on this machine? (y/n)\033[0m
@@ -18,8 +22,10 @@ echo -n -e '''\033[1mIs this the first time of using AMC on this machine? (y/n)\
 Then we will download the python scripts to \033[45m\033[1m~/MC-Projects/'''$exam_name'''\033[0m: '''
 read -r answer_to_q1
 printf %"$COLUMNS"s |tr " " "~"
+
 # Normalise the answers of the user
 sleep 1
+
 # If block to install AMC
 if [[ $answer_to_q1 = "y"* ]] ; then # || [[ $answer_to_q1 = "ye" ]] || [[ $answer_to_q1 = "yes" ]] || [[ -z $answer_to_q1 ]] ; then
     echo -e '\033[1mWe start the automatic setting up\033[0m'
@@ -55,6 +61,7 @@ cd ~/MC-Projects/${exam_name}
 echo -e "\033[1mStarting to clone repository and prepping the files\033[0m"
 sleep 1
 printf %"$COLUMNS"s |tr " " "~"
+
 # Fetching files needed to run the script and prep the LaTeX file: python_for_AMC.py and the wrongCorrect.png
 git clone https://github.com/mirakklys/python_for_AMC.git
 printf %"$COLUMNS"s |tr " " "~"
@@ -64,6 +71,7 @@ sleep 1
 cd ~/MC-Projects/${exam_name}/python_for_AMC/source
 printf %"$COLUMNS"s |tr " " "~"
 echo -e "\033[1mMoving necessary files to \033[45m~/MC-Projects/$exam_name\033[0m \033[1mfolder\033[0m"
+
 # Moving the needed files and deleting all unnecessary stuff
 mv -t ~/MC-Projects/${exam_name} python_for_AMC.py Qs.txt wrongCorrect.png
 mv -t ~/MC-Projects/${exam_name} ~/MC-Projects/${exam_name}/python_for_AMC/source/Empty/*
@@ -74,6 +82,7 @@ echo -e "\033[1mRemoving unnecessary files\033[0m"
 rm -rf python_for_AMC
 printf %"$COLUMNS"s |tr " " "~"
 sleep 1
+
 # Script is executable :)
 chmod +x python_for_AMC.py
 sleep 1
@@ -81,15 +90,19 @@ echo -e "\033[1mJob done!\033[0m"
 sleep 1
 printf %"$COLUMNS"s |tr " " "~"
 echo -n -e '\033[1mNow choose the file with the questions. \033[45m(No need to name it Qs.txt, but it has to be *.txt)\033[0m\n'
+
 # Choose the file with questions
 w_f_m=$(zenity --file-selection)
 sleep 1
+
 # Copy the question into Qs.txt in the folder of the exam 
 cp $w_f_m ~/MC-Projects/${exam_name}/Qs.txt
 sleep 1
+
 # Run the script in the exam folder, so that all the files are in the same place
 cd ~/MC-Projects/${exam_name}
 ~/MC-Projects/${exam_name}/python_for_AMC.py
+
 # Rename the output file ready for compiling LaTeX pdf
 mv prcssdQs.txt $exam_name.txt && rm python_for_AMC.py
 printf %"$COLUMNS"s |tr " " "~"
@@ -97,6 +110,7 @@ sleep 2
 echo -e "\033[1mAll files are processed and cleaned\033[0m"
 sleep 1
 echo -e "\033[1mYou LaTeX source file is called \033[45m"$exam_name".txt\033[0m"
+
 # And goodbye:)
 cd $weAreHere
 sleep 1
